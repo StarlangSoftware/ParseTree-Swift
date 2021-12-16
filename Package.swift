74ca5300ed9d47d2fbc917b011db9d27b932124f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-12-16 18:31:09 +0300

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["ParseTree"]),
     ],
     dependencies: [
-        .package(name: "Dictionary", url: "https://github.com/StarlangSoftware/Dictionary-Swift.git", .exact("1.0.6"))
+        .package(name: "Dictionary", url: "https://github.com/StarlangSoftware/Dictionary-Swift.git", .exact("1.0.8"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
